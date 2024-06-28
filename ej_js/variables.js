a54d6d7a76cfa655a588ee3ac015d709cbc3c4d4
Xavier Pilasi <pilasiha@gmail.com>
2024-06-28 21:18:52 +0200

    updates

---
+++ ej_js/variables.js
@@ -0,0 +1,13 @@
+//Strings
+myName = 'xavi'; 
+lastName = "pilasi";
+userTitle = 'alumno';
+
+console.log(myName);
+
+//Concatenación
+console.log(myName+' '+lastName+ ' '+userTitle);
+console.log(myName,lastName,userTitle);
+
+//Back ticks
+console.log(`Incluir variables: ${myName} ${lastName} -> ${userTitle}`);

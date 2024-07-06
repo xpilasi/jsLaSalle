e72825b7a37b850c23515f7c593838cafe6b1fcb
Xavier Pilasi <pilasiha@gmail.com>
2024-07-06 11:37:17 +0200

    update ejemplos métodos

---
+++ ej_js/metodos.js
@@ -0,0 +1,60 @@
+
+const PI = '3.1415';
+radio = 4.5;
+
+
+radioString = radio.toString();
+radioInt = parseFloat(radioString)
+
+console.log(radioString);
+console.log(radioInt);
+
+console.log(typeof radioString);
+console.log(typeof radioInt);
+
+
+// Métodos: --> Funciones asociadas a un objeto
+
+//Slice
+//To Upper/lower case
+
+
+// trim --> para eliminar los espacios al principio y al final
+
+frase = '   This is Sparta!!  ';
+fraseTrim = frase.trim();
+console.log(frase);
+console.log(fraseTrim);
+
+
+//Pad 
+text = '5';
+console.log(text.padStart(4,'X'));//agrega 3 X
+console.log(text.padStart(4,'0'));//agrega 3 0
+
+//Split
+descripcion = 'Era un día distinto';
+descripcionSplit = descripcion.split(' '); //genera un array
+console.log(descripcionSplit);
+
+//Slice --> Corta desde un punto inicial hasta un punto final
+//No considera el último elemento
+
+frutas = ['uva','sandía','melón','banana'];
+frutasSlice = frutas.slice(1,4);
+console.log(frutasSlice);
+
+
+//Replace: reemplaza solamebte el primero
+
+guitarras = 'Mi marca preferida de guitarras es Gibson y Gibson'
+guitarrasReplace = guitarras.replace('Gibson','Tokai');
+console.log(guitarras);
+console.log(guitarrasReplace);
+
+//Replace all: reemplaza todos
+
+guitarras = 'Mi marca preferida de guitarras es Gibson y Gibson'
+guitarrasReplaceAll = guitarras.replaceAll('Gibson','Tokai');
+console.log(guitarras);
+console.log(guitarrasReplaceAll)
